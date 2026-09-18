b54c9aae99c75267ada2871b920561fa48b9fdd0
Sanika Bhosale <209017016+Sanikabh@users.noreply.github.com>
2026-09-18 18:37:57 +0100

    gc content final code: reads FASTA file

--- gc.py
+++ gc.py
@@ -1,32 +1,29 @@
 sequence = {}
 current_id = ""
 
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    line = line.strip()
-    if line.startswith(">"):
-        current_id = line[1:]
-        sequence[current_id] = ""
-    else:
-        sequence[current_id] += line
+with open('rosalind-datasets/rosalind_gc.txt', "r") as file:
+    for line in file:
+        line = line.strip()
+        if line.startswith(">"):
+            current_id = line[1:]
+            sequence[current_id] = ""
+        else:
+            sequence[current_id] += line
 
-#print(sequence)
-best_id = ""
-best_gc = 0
+    #print(sequence)
+    best_id = ""
+    best_gc = 0
 
-for seq_id, seq in sequence.items():
-    count_GC = 0
-    for base in seq:
-        if base == "G" or base == "C":
-            count_GC += 1
-    gc_content = (count_GC/len(seq)) * 100
-    #print(gc_content)
+    for seq_id, seq in sequence.items():
+        count_GC = 0
+        for base in seq:
+            if base == "G" or base == "C":
+                count_GC += 1
+        gc_content = (count_GC/len(seq)) * 100
+        #print(gc_content)
 
-    if gc_content > best_gc:
-        best_gc = gc_content
-        best_id = seq_id
+        if gc_content > best_gc:
+            best_gc = gc_content
+            best_id = seq_id
 
-print(best_id, best_gc)
+    print(best_id, best_gc)
