9be51f2293a8af505b27e01ad3d0a70f5e7d83f9
Sanika Bhosale <209017016+Sanikabh@users.noreply.github.com>
2026-09-25 21:07:00 +0100

    rna splicing final code

--- splc.py
+++ splc.py
@@ -3,19 +3,16 @@ sequence = {}
 current_id = ""
 first_id = None # capturing first_id
 
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    line = line.strip()
-    if line.startswith(">"):
-        current_id = line[1:]
-        sequence[current_id] = ""
-        if first_id is None:
-            first_id = current_id
-    else:
-        sequence[current_id] += line
+with open("rosalind-datasets/rosalind_splc.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        if line.startswith(">"):
+            current_id = line[1:]
+            sequence[current_id] = ""
+            if first_id is None:
+                first_id = current_id
+        else:
+            sequence[current_id] += line
 
 main_seq = sequence[first_id]
 #print(main_seq)
