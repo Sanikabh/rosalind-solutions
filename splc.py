e36710f5561519745098091cd2aa5edd44c4ba2d
Sanika Bhosale <209017016+Sanikabh@users.noreply.github.com>
2026-09-25 20:35:12 +0100

    capturing the firt_id

--- splc.py
+++ splc.py
@@ -1,6 +1,7 @@
 # RNA splicing
 sequence = {}
 current_id = ""
+first_id = None # capturing first_id
 
 while True:
     try:
@@ -11,6 +12,11 @@ while True:
     if line.startswith(">"):
         current_id = line[1:]
         sequence[current_id] = ""
+        if first_id is None:
+            first_id = current_id
     else:
         sequence[current_id] += line
+
+main_seq = sequence[first_id]
+#print(main_seq)
 
